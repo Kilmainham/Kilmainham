e5390d3139544ac5c3578d24f79390a147068dae
Phil <pohici@tcd.ie>
2015-07-24 12:35:28 +0100

    no real changes

---
+++ Assets/LoadLevelScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+	public string levelToLoad;
+
+	void OnMouseDown() {
+		Application.LoadLevel(levelToLoad);
+	}
+}
