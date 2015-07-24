5cf4735c32b4adfa5b94768b870a62b2fec9f56e
Phil <pohici@tcd.ie>
2015-07-24 14:24:50 +0100

    added scene 2

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+
+		public string levelToLoad;
+		
+		public void ChangeLevel() {
+			Application.LoadLevel(levelToLoad);
+		}
+}
+
