ed230445e7ad343f69f7db611797f330dd1773be
Phil <pohici@tcd.ie>
2015-08-13 14:48:05 +0100

    Butterfly Flying Path integrated into the master branch

--- Assets/ButterflyScripts/FlyingPathScript.cs
+++ Assets/ButterflyScripts/FlyingPathScript.cs
@@ -13,18 +13,19 @@ public class FlyingPathScript : MonoBehaviour {
 		// iterate through all child objects of our flying path
 		foreach (Transform child in transform)
 		{
-			//from the child trasform get the gameObject and from the gameObject get the PathPointScript component
-			PathPointScript pathPointScript = child.gameObject.GetComponent<PathPointScript>();
+			if (child.gameObject.name.Equals("PathPoint")) {
+				//from the child trasform get the gameObject and from the gameObject get the PathPointScript component
+				PathPointScript pathPointScript = child.gameObject.GetComponent<PathPointScript>();
 
-			//check if the pathPoint object has been reached
-			if ( !pathPointScript.hasBeenReached() ){
-				//check if the nextPathPoint has not been set or order of the pathPoint object is less than nextPathPoint's order
-				if (nextPathPointScript == null || pathPointScript.getOrder() < nextPathPointScript.getOrder()) {
-					//if so, pathPoint becomes the next pathPoint
-					nextPathPointScript = pathPointScript;
+				//check if the pathPoint object has been reached
+				if ( !pathPointScript.hasBeenReached() ){
+					//check if the nextPathPoint has not been set or order of the pathPoint object is less than nextPathPoint's order
+					if (nextPathPointScript == null || pathPointScript.getOrder() < nextPathPointScript.getOrder()) {
+						//if so, pathPoint becomes the next pathPoint
+						nextPathPointScript = pathPointScript;
+					}
 				}
 			}
-
 		}
 
 		//Check if nextPathPoint isn't null (i.e. there is a remaining path point to fly to)
---
+++ Assets/ButterflyScripts/TriggerNewFlyingPath.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerNewFlyingPath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name.Equals("Player")) {
+			GameObject butterfly = GameObject.Find ("butterfly");
+			FollowFlyingPath followFlyingPathScript = butterfly.GetComponent<FollowFlyingPath>();
+			followFlyingPathScript.SetFlyingPath(gameObject.transform.parent.gameObject);
+		}
+	}
+}
