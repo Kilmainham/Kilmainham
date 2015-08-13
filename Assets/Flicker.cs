3d53281199598eea6868dc9344fa2fe13230298b
Phil <pohici@tcd.ie>
2015-08-13 16:04:48 +0100

    Candle challenge integrated from clockAnimated

--- Assets/Flicker.cs
+++ Assets/Flicker.cs
@@ -7,8 +7,8 @@ public class Flicker : MonoBehaviour {
 	GameObject candleObject;
 
 	public float speed = 0.01f;
-	float topIntensity = 3.0f;
-	float bottomIntensity = 2.0f;
+	float topIntensity = 2.25f;
+	float bottomIntensity = 1.0f;
 
 	float intensity;
 	// Use this for initialization
