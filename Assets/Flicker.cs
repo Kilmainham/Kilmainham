0924b0805c8665a5c282b976365fa56e30e38124
Phil <pohici@tcd.ie>
2015-08-13 16:07:18 +0100

    Candle challenge integrated from clockAnimated

--- Assets/Flicker.cs
+++ Assets/Flicker.cs
@@ -11,20 +11,19 @@ public class Flicker : MonoBehaviour {
 	float bottomIntensity = 1.0f;
 
 	float intensity;
-	// Use this for initialization
 	void Start () {
 		candleObject = GameObject.Find ("CandleLight");
 		candleLight = candleObject.GetComponent <Light>();
 		intensity = bottomIntensity;
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		candleLight.intensity = intensity;
 		intensity += speed;
 		if (intensity > topIntensity || intensity < bottomIntensity) {
 			speed = speed * -1;
 		}
-		//Debug.Log ("Flickerscript. candleLight.intensity"+candleLight.intensity);
+
 	}
 }
