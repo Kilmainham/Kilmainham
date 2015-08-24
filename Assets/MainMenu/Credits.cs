db6f11a727f1fee55144a0c136e62e625dace27c
Phil <pohici@tcd.ie>
2015-08-24 14:40:51 +0100

    begun adding splash screen and credits

---
+++ Assets/MainMenu/Credits.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+	
+	public float speed = 0.01f;
+	
+	// Update is called once per frame
+	void Update () {
+		float newPositionY = transform.position.y+speed;
+		transform.position = new Vector3(transform.position.x, newPositionY, transform.position.z);		
+
+		if (newPositionY >= 77f) {
+			GameObject screenfader = GameObject.Find("CreditsScreenFader");
+			ScreenFader screenFaderScript = (ScreenFader) screenfader.GetComponent(typeof(ScreenFader));
+			screenFaderScript.startFadeOut();
+			if (screenFaderScript.FadeOutIsComplete()) {
+				Application.LoadLevel("SplashScreen_scene1_menu");
+			}
+		}
+	}
+}
--- Assets/ScreenFader/ScreenFader.cs
+++ Assets/ScreenFader/ScreenFader.cs
@@ -2,17 +2,19 @@
 using System.Collections;
 
 public class ScreenFader : MonoBehaviour {
-	public GameObject fader;
+	//public GameObject fader;
 	Renderer rend;
 	public Color faderColor;
 	public float alpha = 1.0f;
 	public bool fadingIn = true;
 	public bool fadingOut = false;
 	// Use this for initialization
+	
 	void Start () {
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
-		fader = GameObject.Find ("ScreenFader");
-		rend = fader.GetComponent<Renderer> ();
+		//fader = GameObject.Find ("ScreenFader");
+		//rend = fader.GetComponent<Renderer> ();
+		rend = gameObject.GetComponent<Renderer> ();
 		rend.enabled = true;
 	}
 	
@@ -20,15 +22,15 @@ public class ScreenFader : MonoBehaviour {
 	void Update () {
 		if(fadingIn){
 			fadeIn();				
-			}
+		}
 		if (fadingOut) {
 			fadeOut ();
 		}
-
+		
 	}
-
+	
 	void fadeIn(){
-
+		
 		faderColor = new Color (0f, 0f, 0f, alpha);
 		alpha = alpha - 0.01f;
 		rend.material.color = faderColor;
@@ -36,11 +38,11 @@ public class ScreenFader : MonoBehaviour {
 			rend.enabled=false;
 			fadingIn=false;
 		}
-
+		
 	}
-
+	
 	void fadeOut(){
-
+		
 		rend.enabled = true;
 		faderColor = new Color (0f, 0f, 0f, alpha);
 		rend.material.color = faderColor;
@@ -50,5 +52,22 @@ public class ScreenFader : MonoBehaviour {
 	}
 	public void startFadeOut(){
 		fadingOut = true;
+		fadingIn = false;
+	}
+	
+	public void startFadeIn(){
+		fadingIn = true;
+		fadingOut = false;
+	}
+	
+	//return true if fadeIn is complete
+	public bool FadeInIsComplete () {
+		return alpha <=0f;
+		
+	}
+	// return true if fadeOut is complete
+	public bool FadeOutIsComplete () {
+		return alpha >=1f;
+		
 	}
 }
